95d09951d3cb36c36799fa82d34a0a92edfd4320
Jonathan St-Onge <jonathanstonge7@gmail.com>
2024-05-28 13:52:29 -0400

    s2orc collection updated

--- src/import/create_s2orc_dark_data.py
+++ src/import/create_s2orc_dark_data.py
@@ -1,11 +1,15 @@
 # import jsonlines
 import pandas as pd
+import re
+# from bson import json_util
 import json
-# import spacy
+import spacy
 
 from pathlib import Path
 from tqdm import tqdm
 import argparse
+
+from pymongo import MongoClient
 
 def parse_args():
     parser = argparse.ArgumentParser("Data Downloader")
@@ -17,8 +21,112 @@ def parse_args():
     )
     return parser.parse_args()
 
+def get_journals_display_name(db, df_google):
+    df_oa = pd.DataFrame(list(db.venues_oa.find( {}, 
+        {"display_name": 1, "alternate_titles":1, "abbreviated_title": 1, "ids": 1}
+    )))
+    
+    oa_display_name2googlepub = {
+        'Review of Financial Studies': 'the review of financial studies',
+        'Applied Catalysis B-environmental': 'applied catalysis b: environmental',
+        'Journal of energy & environmental sciences': 'energy & environmental science',
+        'Journal of materials chemistry. A, Materials for energy and sustainability': 'journal of materials chemistry a',
+        'The American Economic Review': 'american economic review',
+        'Quarterly Journal of Economics': 'the quarterly journal of economics',
+        'Journal of Finance': 'the journal of finance',
+        'Physical Review X': 'physical review. x',
+        'European Physical Journal C': 'the european physical journal c',
+        'Nature Reviews Molecular Cell Biology': 'nature reviews. molecular cell biology',
+        'Journal of Religion & Health': 'journal of religion and health',
+        'Lancet Oncology': 'the lancet oncology',
+        'Lancet Infectious Diseases': 'the lancet infectious diseases',
+        'Astronomy and Astrophysics': 'astronomy & astrophysics',
+        'Light-Science & Applications': 'light: science & applications',
+        'Energy research and social science': 'energy research & social science',
+        'Global Environmental Change-human and Policy Dimensions': 'global environmental change',
+        'Journalism: Theory, Practice & Criticism': 'journalism',
+    }
 
-def create_s2orc_dark_data(db, tot_venue_set):
+    # remove duplicated? Why do we have duplicated in our db.
+    df_oa = df_oa[~df_oa.display_name.duplicated()]
+
+    # get everything lower case wihtout changing display_name.
+    def lower_display_name(x):
+        return [oa_display_name2googlepub[x].lower() 
+                if oa_display_name2googlepub.get(x) 
+                else x.lower() 
+                for x in x.display_name]
+    
+    df_oa = df_oa.assign(display_name_lower = lower_display_name)
+
+    df_oa['issn'] = df_oa.ids.map(lambda x: x['issn'][0] if x.get('issn') else None)
+
+    return df_google.merge(df_oa, how="left", left_on="Publication", right_on="display_name_lower")
+
+def add_journals_via_issnl(db, metadata_venue, df_google):
+    # missing venues, if anyone wants to keep looking for them in the db.
+    # mostly coming from computer science journals.
+    missing_venues = metadata_venue[metadata_venue.display_name.isna()]
+    metadata_venue = metadata_venue[~metadata_venue.display_name.isna()]
+    
+    # oa_issnl2googlepub = {
+    #     '1063-6919', 'ieee/cvf conference on computer vision and pattern recognition',
+    #     '1364-0321', 'renewable and sustainable energy reviews'
+    #     '2159-5399', 'aaai conference on artificial intelligence',
+    #     '1520-6149', 'ieee international conference on acoustics, speech and signal processing (icassp)'
+    # }
+    
+    # last one missing
+    # '1550-5499', 'ieee/cvf international conference on computer vision',
+
+    list_issnl = ['1063-6919', '1364-0321', '2159-5399', '1520-6149']
+    
+    df_oa_issnl = pd.DataFrame(list(db.venues_oa.find(
+        {"issn_l": {"$in": list_issnl}}, 
+        {"display_name": 1, "alternate_titles":1, "abbreviated_title": 1, "ids": 1, 'issn_l': 1}
+    )))
+    
+    missing_metadata_venue = df_google.loc[(df_google.Publication.isin(missing_venues.Publication.tolist())) & (df_google.Publication != 'ieee/cvf international conference on computer vision'),:]\
+             .assign(issn_l = list_issnl)\
+             .merge(df_oa_issnl, how="left", on="issn_l")
+
+    return pd.concat([metadata_venue, missing_metadata_venue], axis=0).reset_index(drop=True)
+
+def main():
+    args = parse_args()
+    INPUT_DIR = args.input
+    OUTPUT_DIR = args.output
+
+    from pymongo import MongoClient
+
+    pw = "password"
+    uri = f"mongodb://cwward:{pw}@wranglerdb01a.uvm.edu:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+    client = MongoClient(uri)
+
+    db = client['papersDB']
+    
+    df_google = pd.read_csv(INPUT_DIR / "journalsbyfield.csv", usecols=['Publication', 'h5-index', 'h5-median'])\
+                  .assign(Publication = lambda x: x.Publication.str.lower())
+    
+    meta_f = Path(INPUT_DIR / "metadata_venue.csv")
+    metadata_venue = pd.read_csv(meta_f) if meta_f.exists() else get_journals_display_name(db, df_google)
+    metadata_venue = add_journals_via_issnl(db, metadata_venue, df_google)
+    # metadata_venue.to_csv("metadata_venue.csv", index=False)
+    
+    #!TODO: take care of duplication
+    metadata_venue = metadata_venue[~metadata_venue.display_name.duplicated()]
+    
+    # dropping nas. 170/180 left.
+    # metadata_venue = metadata_venue[~metadata_venue.display_name.isna()]
+
+    # Grab a random sample of metadata about papers from mongoDB.
+    # The issue here is that we do not know the field of study just by looking at papers,
+    # so we need to identify the corpusIDs given the field first. 
+
+    # We are using display name instead of issn because few journals/conferene do not seem to have
+    # issn. At this point, display name should work fine though.
+    tot_venue_set = set(metadata_venue.display_name)
+    
     nlp = spacy.load("en_core_web_trf", enable=["tok2vec"])
 
     i = 0 
@@ -26,8 +134,6 @@ def create_s2orc_dark_data(db, tot_venue_set):
         # venue = 'Applied Linguistics'
         # venue = list(tot_venue_set)[0]        
         print(f"doing {venue} (done {i}/{len(tot_venue_set)})")
-
-        # get_relevant_cids
         venue_df = pd.DataFrame(list(db.papers.aggregate([
             { "$match": { 
                 'works_oa.host_venue.display_name': venue, 
@@ -89,103 +195,6 @@ def create_s2orc_dark_data(db, tot_venue_set):
                 db.s2orc_dark_data.insert_many( text_subset ) 
 
         i += 1
-
-def main():
-    args = parse_args()
-    INPUT_DIR = args.input
-    OUTPUT_DIR = args.output
-
-    # Loading data from venues google
-
-    venues_from_google = pd.read_csv(INPUT_DIR / "journalsbyfield.csv", usecols=['Publication', 'h5-index', 'h5-median'])
-    venues_from_google["normalized_name"] = venues_from_google.Publication.str.lower()
-    venues_from_google = venues_from_google[~venues_from_google.normalized_name.duplicated()]
-
-    # Matching papers from google onto OpenAlex -------------------------------
-
-    db = client['papersDB']
-
-    df_oa = pd.DataFrame(list(db.venues_oa.find( {}, 
-            {"display_name": 1, 
-            "alternate_titles":1, 
-            "abbreviated_title": 1, 
-            "ids": 1}
-        )))
-
-    df_oa = df_oa[~df_oa.display_name.duplicated()] # 221K different journals
-
-    # Finding venues that mismatch
-    oa_display_name2googlepub = {
-            'Review of Financial Studies': 'the review of financial studies',
-            'Applied Catalysis B-environmental': 'applied catalysis b: environmental',
-            'Journal of energy & environmental sciences': 'energy & environmental science',
-            'Journal of materials chemistry. A, Materials for energy and sustainability': 'journal of materials chemistry a',
-            'The American Economic Review': 'american economic review',
-            'Quarterly Journal of Economics': 'the quarterly journal of economics',
-            'Journal of Finance': 'the journal of finance',
-            'Physical Review X': 'physical review. x',
-            'European Physical Journal C': 'the european physical journal c',
-            'Nature Reviews Molecular Cell Biology': 'nature reviews. molecular cell biology',
-            'Journal of Religion & Health': 'journal of religion and health',
-            'Lancet Oncology': 'the lancet oncology',
-            'Lancet Infectious Diseases': 'the lancet infectious diseases',
-            'Astronomy and Astrophysics': 'astronomy & astrophysics',
-            'Light-Science & Applications': 'light: science & applications',
-            'Energy research and social science': 'energy research & social science',
-            'Global Environmental Change-human and Policy Dimensions': 'global environmental change',
-            'Journalism: Theory, Practice & Criticism': 'journalism',
-        }
-
-    # get everything lower case wihtout changing display_name.
-    def lower_display_name(x):
-        return [oa_display_name2googlepub[x].lower() 
-                if oa_display_name2googlepub.get(x) 
-                else x.lower() 
-                for x in x.display_name]
-
-    df_oa = df_oa.assign(normalized_name = lower_display_name)
-
-    # add issn as a column
-    df_oa['issn'] = df_oa.ids.map(lambda x: x['issn'][0] if x.get('issn') else None)
-
-    # Match venues from OpenAlex onto Google ones based on normalized_name
-    # Note that from now on we have "duplicated entry" who have the same names
-    # but different ISSNs. Annoying. We'll drop them later.  
-    venues_from_oa = venues_from_google.merge(
-        df_oa, 
-        how="left", 
-        on="normalized_name", 
-    )
-
-    # Venues we failed to match. We'll use ISSN-L instead of name
-    missing_venues = venues_from_oa[venues_from_oa.display_name.isna()]
-    metadata_venue = venues_from_oa[~venues_from_oa.display_name.isna()]
-
-    # Find issn-l of those venues
-    list_issnl = ['1063-6919', '1364-0321', '2159-5399', '1520-6149']
-
-    # Find if they exists in our DB  
-    df_oa_issnl = pd.DataFrame(list(db.venues_oa.find(
-            {"issn_l": {"$in": list_issnl}}, 
-            {"display_name": 1, "alternate_titles":1, "abbreviated_title": 1, "ids": 1, 'issn_l': 1}
-        )))
-
-    missing_metadata_venue = venues_from_google.loc[(venues_from_google.normalized_name.isin(missing_venues.normalized_name.tolist())) & (venues_from_google.normalized_name != 'ieee/cvf international conference on computer vision'),:]\
-                .assign(issn_l = list_issnl)\
-                .merge(df_oa_issnl, how="left", on="issn_l")
-
-    # Rewrite venues_from_oa with everything
-    venues_from_oa = pd.concat([metadata_venue, missing_metadata_venue], axis=0).reset_index(drop=True)
-
-    # By dropping duplicated normalized_name we get to 142/170...
-    # We originally had 153 distinct venues from Google.
-    venues_from_oa = venues_from_oa[~venues_from_oa.normalized_name.duplicated()]
-
-    # We are using display name instead of issn because few journals/conferene do not seem to have
-    # issn. At this point, display name should work fine though.
-    tot_venue_set = set(metadata_venue.display_name)
-    
-    create_s2orc_dark_data(db, tot_venue_set)
 
 
 if __name__ == '__main__':
