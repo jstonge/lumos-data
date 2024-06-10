bb5422c1f45881e4ae8102c13e141aebb43941b9
Jonathan St-Onge <jonathanstonge7@gmail.com>
2024-06-10 09:17:01 -0400

    update label_studio class

--- Makefile
+++ Makefile
@@ -6,27 +6,46 @@ DATA_DIR_KSPLIT=$(DATA_DIR)/DATA_DIR_KSPLIT # trainin data
 
 SRC_DIR=./src
 
+# run once to create the dark data on label studio
+create-dark-data-LS:
+	cd src && python -c "from label_studio import labelStudio; LS = labelStudio(); LS.create_project(LS.dark_data_project)"
+
 # Scrape google scholar to control for different field of studies
 import-gscholar-jns:
 	python $(SRC_DIR)/import/scrape_gscholar.py -o $(DATA_DIR_JNS)
 
 # We create a collection in mongoDB where each document is 
-# a section containing a mention of data. The sections come from
-# papers from selected journals that were in the s2orc database.
+# a section of papers from selected venues.
 create-s2orc-dark-data-collection:
 	python $(SRC_DIR)/import/create_s2orc_dark_data_collection.py -i $(DATA_DIR_JNS) 
 
-# We use clusteredDataStatement as main file for the annotations
-# I don't know where the data comes from, but it is in the data folder...
-cluster-data:
-	python $(SRC_DIR)/process/cluster_data.py -i $(DATA_DIR_JNS) -o $(DATA_DIR_PROCESSED)
 
-assign-data-round-1:
-	python $(SRC_DIR)/annotations/2023-03-07-assign-data.py -i $(DATA_DIR_PROCESSED) -o $(DATA_DIR_ANNOTS)
-	
-assign-data-round-2:
-	python $(SRC_DIR)/import/2023-09-21-assign-data.py -i $(processed) -o $(DATA_DIR_ANNOTS)
+more-data-annotations:
+	cd src && python -c "from label_studio import labelStudio; LS = labelStudio(); LS.dispatch_annots(keyword='data')"
+
+more-code-annotations:
+	cd src && python -c "from label_studio import labelStudio; LS = labelStudio(); LS.dispatch_annots(keyword='code')"
+
+more-software-annotations:
+	cd src && python -c "from label_studio import labelStudio; LS = labelStudio(); LS.dispatch_annots(keyword='software')"
 
 # We train-test split the data, somewhere. I don't remember where...
 create-train-test:
-	python $(SRC_DIR)/ksplit/train_test.py -i $(DATA_DIR_ANNOTS) -o $(DATA_DIR_KSPLIT)
+	cd src && python $(SRC_DIR)/train_test.py
+
+fine-tune-llama3:
+	tune run --nproc_per_node 4 lora_finetune_distributed --config ./my_custom_config.yaml dtype=fp32
+
+# Next 3 steps are now obsolete because we use label studio. 
+
+# We use clusteredDataStatement as main file for the annotations.
+# We lost track of the script producing the data file...
+# cluster-data:
+# 	python $(SRC_DIR)/process/cluster_data.py -i $(DATA_DIR_JNS) -o $(DATA_DIR_PROCESSED)
+
+# original round of annotations using excel sheet
+# assign-data-round-1:
+# 	python $(SRC_DIR)/annotations/2023-03-07-assign-data.py -i $(DATA_DIR_PROCESSED) -o $(DATA_DIR_ANNOTS)
+
+# assign-data-round-2:
+# 	python $(SRC_DIR)/import/2023-09-21-assign-data.py -i $(processed) -o $(DATA_DIR_ANNOTS)
--- src/label_studio.py
+++ src/label_studio.py
@@ -29,26 +29,34 @@ class labelStudio:
         self.db = client['papersDB']
         self.cache = Path("./cache")
         self.annotators = {'juniper.lovato@uvm.edu': 19456, 'achawla1@uvm.edu': 17284, 'CW': 23575, 'JZ': 23576, 'jonathan.st-onge@uvm.edu': 16904}
-        self.active_annotators = {'juniper.lovato@uvm.edu':19456, 'achawla1@uvm.edu': 17284, 'jonathan.st-onge@uvm.edu': 16904}
+        # self.active_annotators = {'juniper.lovato@uvm.edu':19456, 'achawla1@uvm.edu': 17284, 'jonathan.st-onge@uvm.edu': 16904}
+        self.active_annotators = {'jonathan.st-onge@uvm.edu': 16904}
 
         if self.cache.exists() == False:
             self.cache.mkdir()
 
         print('accessing project status...')
-        project_status = self.is_dark_data_project_exists()
+        project_status = self.is_project_exists('Dark-Data')
+        code_proj_status = self.is_project_exists('Code-Statement')
+        
         if project_status is None:
             self.proj_id = self.create_dark_data_project()
         else:
             self.proj_id = project_status
+        
+        if code_proj_status is None:
+            self.code_proj_id = self.create_code_project()
+        else:
+            self.code_proj_id = code_proj_status
 
     # LABEL STUDIO HELPERS
 
-    def get_annotations(self, only_annots=True):
+    def get_annotations(self, proj_id, only_annots=True):
         """Get annotations of a given project id."""
         headers = { "Authorization": f"Token {self.LS_TOK}" }
         base_url = "https://app.heartex.com/api/projects"
         
-        url = f"{base_url}/{self.proj_id}/export?exportType=JSON&download_all_tasks=true"
+        url = f"{base_url}/{proj_id}/export?exportType=JSON&download_all_tasks=true"
         print("requesting the annots...")
         response = requests.get(url, headers=headers)
 
@@ -63,79 +71,24 @@ class labelStudio:
         else:
             print(f"Failed to fetch data: {response.status_code}")
 
-    def post_LS(self, data: List) -> None:
+    def post_LS(self, proj_id:int, data: List) -> None:
         """Import data to label studio"""
-        response = requests.post(f'https://app.heartex.com/api/projects/{self.proj_id}/import', 
+        response = requests.post(f'https://app.heartex.com/api/projects/{proj_id}/import', 
                                 headers={'Content-Type': 'application/json', 'Authorization': f"Token {self.LS_TOK}"}, 
                                 data=json.dumps(data), verify=False)
         print(response.status_code)
 
     # PROJECT HELPERS
 
-    def is_dark_data_project_exists(self) -> int:
+    def is_project_exists(self, title: str) -> int:
         """if it exists, return project id, else return None"""
         headers = { "Authorization": f"Token {self.LS_TOK}"}
         response = requests.get("https://app.heartex.com/api/projects", headers=headers)
         if response.status_code == 200:
             all_projects = json.loads(response.text)['results']
-            proj_id = [_['id'] for _ in all_projects if _['title'] == 'Dark-Data']
+            proj_id = [_['id'] for _ in all_projects if _['title'] == title]
             if len(proj_id) > 0:
                 return proj_id[0]
-
-    # MONGODB HELPERS
-
-    def read_cache_venue(self, N=500) -> pd.DataFrame:
-        """read cache of venues with a certain threshold"""
-        cache_f = self.cache / f"venues_{N}.csv"
-        if cache_f.exists():
-            return pd.read_csv(cache_f)
-
-    def get_venues_more_than_N(self, keyword: str = "data", N:int = 500) -> List[str]:
-        """return venues with more than N occurences of a given keyword in text."""
-        # the pipeline below takes time. We use caching when we can. 
-        list_venues = self.read_cache_venue(N=N)
-        
-        if list_venues is None:
-            print("no cached filed found, querying mongoDB...")
-            pipeline = [
-                { "$match": {"text": keyword}  },
-                {
-                    '$group': {
-                        '_id': { 'venue': '$venue' }, 
-                        'count': { '$sum': 1 }
-                    }
-                },
-                {
-                    "$match": {
-                        "count": {"$gt": N}
-                } 
-                },
-                {
-                    "$project" : {
-                        "_id" : '$_id.venue'
-                }
-                }
-            ]
-            
-            list_venues = pd.DataFrame(list(self.db.s2orc_dark_data.aggregate(pipeline)))
-        
-        return list(set(list_venues['_id'].sort_values().tolist()))
-        
-    def get_sample_jn(self, jn:str, size:int, done_ids:Set = set()) -> pd.DataFrame:
-        pipeline = [
-                { "$match":  { "venue": jn, "text": "data" }   } ,
-                { "$sample": { "size": size } }
-        ]
-        print("querying DB for more pubs...")
-        hits = pd.DataFrame(list(self.db.s2orc_dark_data.aggregate(pipeline)))
-        
-        if len(hits) > 0:
-            return hits[~hits['corpusid'].isin(done_ids)]
-        else:
-            print("no more hits")
-
-        
-    # CREATE PROJECT
 
     def create_dark_data_project(self) -> int:
         """create dark data project"""
@@ -162,27 +115,127 @@ class labelStudio:
         if response.text == 200:
             print("project created")
             return json.loads(response.text)['id']
+    
+    def create_code_project(self) -> int:
+        """create code project. Based on https://codalab.lisn.upsaclay.fr/competitions/16935"""
+        if self.is_project_exists('Code-Statement'):
+            return "project already exists"
+        
+        project_config = """\
+            {"title": "Code-Statement","label_config": "<View>\
+            <View>\
+            <Text name='text' value='$text'/>\
+            <Choices name='sentiment' toName='text'>\
+                <Choice value='usage'/>\
+                <Choice value='creation'/>\
+                <Choice value='mention'/>\
+                <Choice value='unclear'/>\
+                <Choice value='None'/>\
+            </Choices>\
+            </View>\
+            </View>"}
+            """
+    
+        response = requests.post(f'https://app.heartex.com/api/projects', 
+                         headers={'Content-Type': 'application/json', 'Authorization': f"Token {self.LS_TOK}"}, 
+                         data=project_config, verify=False)
 
+        print(response.status_code)
+
+        if response.text == 200:
+            print("project created")
+            return json.loads(response.text)['id']
+
+
+    # MONGODB HELPERS
+
+    def read_cache_venue(self, keyword: str, N=500) -> pd.DataFrame:
+        """read cache of venues with a certain threshold"""
+        cache_f = self.cache / f"venues_{keyword}_{N}.csv"
+        if cache_f.exists():
+            return pd.read_csv(cache_f)
+
+    def get_venues_more_than_N(self, keywords: List[str] = ["data"], N:int = 500) -> List[str]:
+        """return venues with more than N occurences of a given keyword in text."""
+        # the pipeline below takes time. We use caching when we can. 
+        list_venues = self.read_cache_venue('_'.join(keywords), N=N)
+        
+        if list_venues is None:
+            print("no cached filed found, querying mongoDB...")
+            pipeline = [
+                { "$match": {"text": {"$in": keywords}}  },
+                {
+                    '$group': {
+                        '_id': { 'venue': '$venue' }, 
+                        'count': { '$sum': 1 }
+                    }
+                },
+                {
+                    "$match": {
+                        "count": {"$gt": N}
+                } 
+                },
+                {
+                    "$project" : {
+                        "_id" : '$_id.venue'
+                }
+                }
+            ]
+            
+            list_venues = pd.DataFrame(list(self.db.s2orc_dark_data.aggregate(pipeline)))
+            
+            #write to cache
+            list_venues.to_csv(self.cache / f"venues_{'_'.join(keywords)}_{N}.csv", index=False)
+
+        return list(set(list_venues['_id'].sort_values().tolist()))
+        
+    def get_sample_jn(self, keywords:str, jn:str, size:int, done_ids:Set = set()) -> pd.DataFrame:
+        """get a sample of size from a given journal. Exclude done_ids."""
+        pipeline = [
+                { "$match":  { "venue": jn, "text": {"$in": keywords} }   } ,
+                { "$sample": { "size": size } }
+        ]
+        print("querying DB for more pubs...")
+        hits = pd.DataFrame(list(self.db.s2orc_dark_data.aggregate(pipeline)))
+        
+        if len(hits) > 0:
+            return hits[~hits['corpusid'].isin(done_ids)]
+        else:
+            print("no more hits")
+        
     # WORKING WITH LABEL STUDIO
     
-    def more_annotations(self, min_wc:int = 5, max_wc:int = 1000, sample_by_field:int = 1000) -> pd.DataFrame:
+    def more_annotations(self, proj_id: int = 70656, only_annots=False, min_wc:int = 5, max_wc:int = 1000, sample_by_field:int = 1000) -> pd.DataFrame:
+        """
+        get more annotations for a given keyword.
+
+        Parameters:
+         - only_annots: if True, filter out annotated tasks else all tasks on Label Studio
+         - min_wc: minimum word count of the text
+         - max_wc: maximum word count of the text  
+         - sample_by_field: number of samples to get per venue
+        """
         print("get already doing annotations...")
-        done_annots = self.get_annotations()
+        
+        done_annots = self.get_annotations(proj_id, only_annots=only_annots)
         done_corpusids = set([_['data']['corpusid'] for _ in done_annots]) if len(done_annots) > 0 else set()
         
-        print("getting venues with more than 2000 occurences of 'data'...")
-        all_jns_gt_thresh = self.get_venues_more_than_N(N=2000)
+        overshoot_sample = sample_by_field*2 # there must be a better way to do this. Rn we sample way more for DB to make sure we have
+                                             # enough hits by keyword. This is a bit wasteful. 
+
+        # We first get venues for which we have overshoot samples
+        keywords = ['data'] if proj_id == 70656 else ['code', 'software']
+        print(f"getting venues with more than 2000 occurences of '{keywords}'...")
+        all_jns_gt_thresh = self.get_venues_more_than_N(keywords=keywords, N=overshoot_sample)
         
-        # We get more annotations for each venue
+        # Then we get more annotations for each venue
         out = []
-        overshoot_sample = sample_by_field*2 # there must be a better way to do this
         for jn in all_jns_gt_thresh:
-            out.append(self.get_sample_jn(jn=jn, size=overshoot_sample, done_ids=done_corpusids))
+            out.append(self.get_sample_jn(keywords, jn, size=overshoot_sample, done_ids=done_corpusids))
         new_annots = pd.concat(out, axis=0)
 
         # just making sure we don't have any overlap
         assert len(set(new_annots.corpusid.unique()) & done_corpusids) == 0
-        
         
         # We filter by word count
 
@@ -197,12 +250,13 @@ class labelStudio:
         
         return subset_d
     
-    def dispatch_annots(self):
-        annots_to_dispatch = self.get_annotations(min_wc=5, max_wc=1000, sample_by_field=500)
+    def dispatch_annots(self, proj_id:int = 70656, N:int = 200):
+        """dispatch N annotations to each (active) annotator."""
+        annots_to_dispatch = self.more_annotations(proj_id=proj_id)
 
         for email, annot_id in self.active_annotators.items():
             print(f"dispatching to {email}")
-            next_corpus_id = np.random.choice(annots_to_dispatch.corpusid_unique, 3000)
+            next_corpus_id = np.random.choice(annots_to_dispatch.corpusid_unique, N)
             next_sample_df = annots_to_dispatch[annots_to_dispatch.corpusid_unique.isin(next_corpus_id)]
 
             data2dump = []
@@ -240,9 +294,49 @@ class labelStudio:
 
         annots_to_dispatch = annots_to_dispatch[~annots_to_dispatch.corpusid_unique.isin(next_corpus_id)]
 
+    def preannotate_llama3(self):
+        
+
 
 
 # LS = labelStudio()
-# new_annots = LS.more_annotations()
-# new_annots.value_counts("venue")
+# new_annots = LS.more_annotations(72804)
+# for email, annot_id in LS.active_annotators.items():
+#     print(f"dispatching to {email}")
+#     next_corpus_id = np.random.choice(new_annots.corpusid_unique, 200)
+#     next_sample_df = new_annots[new_annots.corpusid_unique.isin(next_corpus_id)]
+
+#     data2dump = []
+#     for i, row in next_sample_df.iterrows():
+        
+#         data2dump.append({
+#             "data": {
+#                 'corpusid': row['corpusid'],
+#                 'corpusid_unique': row['corpusid_unique'],
+#                 'par_id': row['par_id'],
+#                 'wc': row['wc'],
+#                 'text': row['text']
+#             },
+#             "annotations": [{
+#                 'completed_by' : {
+#                     "id": annot_id,
+#                     "first_name": "",
+#                     "last_name": "",
+#                     "avatar": None,
+#                     "email": email,
+#                     "initials": email[:2]
+#                 },
+#                 'result': [{
+#                     'value': {'choices': []},
+#                     'id': "",
+#                     "from_name": 'sentiment',
+#                     'to_name': 'text',
+#                     'type': 'choices',
+#                     'origin': 'manual',
+#                 }]
+#                 }]
+#         })     
     
+#     LS.post_LS(data2dump)
+    
+#     new_annots = new_annots[~new_annots.corpusid_unique.isin(next_corpus_id)]
